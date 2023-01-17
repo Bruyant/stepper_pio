bc368befb7430c4576691550faa0e50ab5b54076
Nicolas Bruyant <nicolas.bruyant@lncmi.cnrs.fr>
2023-01-17 16:40:23 +0100

    set pin to the ones used on the board

--- code.py
+++ code.py
@@ -2,8 +2,8 @@ import stepper
 import board
 
 s = stepper.Stepper(
-    board.D6, limit_pins=(board.D0, board.D1), dir_pin=board.D4, encoder_pis=(board.D2, board.D3)
-)
+    board.GP13, limit_pins=(board.GP10, board.GP11),
+    dir_pin=board.GP12, count_pin=board.GP3)
 s.dir_active = "HIGH"
 s._setup_sm()
 
